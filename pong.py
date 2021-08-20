029cb04bcec7d7e03f3a8e257c52349a654e99bb
jordanalexis6 <jordan.a.neill@gmail.com>
2021-08-20 12:09:05 -0500

    added if statement for when the ball hits the top border

--- pong.py
+++ pong.py
@@ -74,6 +74,13 @@ wn.onkeypress(paddle_b_down, "Down")
 while True:
     wn.update()
 
-    # Move the ball
+    # move the ball
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
+
+    # border checking
+
+    # top & bottom
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
