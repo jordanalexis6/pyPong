98366e2997535bc1ff9adc20637f64cfd50c85b9
jordanalexis6 <jordan.a.neill@gmail.com>
2021-08-19 20:45:12 -0500

    forgot @ next to my user name

--- pong.py
+++ pong.py
@@ -3,7 +3,7 @@ import turtle
 
 # window
 wn = turtle.Screen()
-wn.title("Pong by jordanalexis6")
+wn.title("Pong by @jordanalexis6")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
